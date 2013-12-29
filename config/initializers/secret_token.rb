7bd7f9ed0bc17b1160ce4bd5d490418fb51d5482
Paolo Cosentino <cosepaol88@gmail.com>
2013-12-29 00:00:04 -0600

    Initialize Repo Pete Profile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetesProfile::Application.config.secret_token = 'bbce7027b1863df209bb0ec13f51d2b0897921e3ff9427dcd8d181e18953d3dcad3d6642a87029e3e64bd7d86655d9caffb1bad5e276af98c57acca7b9c5dd9c'
