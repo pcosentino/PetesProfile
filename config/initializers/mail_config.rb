450fadf7e35547868da35340dacfffcff08e33d8
Paolo Cosentino <cosepaol88@gmail.com>
2013-12-30 12:27:33 -0600

    added user mailer

---
+++ config/initializers/mail_config.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+	:address              => "smtp.gmail.com",
+	:port                 => 587,
+	:domain               => "gmail.com",
+	:user_name            => 'pcportfolio88@gmail.com',
+	:password             => 'ilovelucy88',
+	:authentication       => 'plain',
+	:enable_starttls_auto => true  }
